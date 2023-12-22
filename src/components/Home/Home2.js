7b6530adfe6ff30af822c971384b811ef79c83b9
sejin0104 <121476010+sejin0104@users.noreply.github.com>
2023-12-23 08:50:12 +0900

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,21 +19,20 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I feel like my life is already set on being a developer...
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python, Node.js</b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Web Technologies and Programming language </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  Backend(Server)
                 </b>
               </i>
               <br />
