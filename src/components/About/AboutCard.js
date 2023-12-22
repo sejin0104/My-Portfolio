cc6b6b4b4ef91ac80eb58c30fa7d53c27a611dd8
sejin0104 <121476010+sejin0104@users.noreply.github.com>
2023-12-23 08:27:46 +0900

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,26 +8,25 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">SEJINJIN </span>
+            from <span className="purple"> Busan, Republic of Korean</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am student
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I haven't graduated from college yet
             <br />
             <br />
-            Apart from coding, some other activities that I love to do!
+            I have various hobbies besides coding.
           </p>
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Writing My Blogs
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Study New Programm Languages
             </li>
           </ul>
 
