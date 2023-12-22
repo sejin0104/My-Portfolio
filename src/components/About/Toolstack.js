b06e7b8bfca6b23a0468c0ed5b5b0898dd220181
sejin0104 <121476010+sejin0104@users.noreply.github.com>
2023-12-23 08:37:53 +0900

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -18,12 +18,6 @@ function Toolstack() {
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
       </Col>
     </Row>
